e7d9b982c1e9b8dced40968fa8a5593bd55832b1
liuseki <ld@meng2333.com>
2021-07-27 13:25:26 +0800

    中间件-逻辑修改

--- core/middlewares/timer.py
+++ core/middlewares/timer.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 import time
-from contextvars import ContextVar, Token
 from starlette.requests import Request
 from starlette.responses import Response
 from loguru import logger
@@ -12,14 +11,13 @@ from .base import BaseMiddleware
 class TimerMiddleware(BaseMiddleware):
 
     threshold: float = 1.0
-    time_var: ContextVar = ContextVar('time')
 
     async def before_request(self, request: Request) -> None:
-        request.state.time_token = self.time_var.set(time.time())
+        request.state.start_time = time.time()
 
     async def before_response(self, request: Request, response: Response) -> None:
-        if hasattr(request.state, 'time_token'):
-            diff = time.time() - self.time_var.get()
+        key: str = 'start_time'
+        if hasattr(request.state, key):
+            diff: float = time.time() - getattr(request.state, key)
             if diff > self.threshold:
                 logger.warning('response timeout: %.6f' % diff)
-            self.time_var.reset(request.state.time_token)
